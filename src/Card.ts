144b680f06fba4e2dcbea4ec19b55a74498e5676
Paul Sonnentag <paul.sonnentag@googlemail.com>
2025-03-16 16:29:35 +0100

    allow to draw cards and drag them around

---
+++ src/Card.ts
@@ -0,0 +1,83 @@
+import { v4 as uuid } from "uuid";
+import { EventEmitter } from "eventemitter3";
+
+type CardEvents = {
+  pointerdown: (event: PointerEvent, card: Card) => void;
+  click: (event: MouseEvent, card: Card) => void;
+};
+
+type CardConfig = {
+  x?: number;
+  y?: number;
+  width?: number;
+  height?: number;
+};
+
+export class Card extends EventEmitter<CardEvents> {
+  id: string = uuid();
+
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+
+  isSelected: boolean = false;
+  element: HTMLElement;
+
+  children: Card[] = [];
+
+  constructor(config: CardConfig) {
+    super();
+    this.x = config.x ?? 0;
+    this.y = config.y ?? 0;
+    this.width = config.width ?? 100;
+    this.height = config.height ?? 100;
+    this.onPointerDown = this.onPointerDown.bind(this);
+    this.onClick = this.onClick.bind(this);
+
+    this.element = document.createElement("div")!;
+    this.element.addEventListener("pointerdown", this.onPointerDown);
+    this.element.addEventListener("click", this.onClick);
+    this.reconcile();
+  }
+
+  onPointerDown(event: PointerEvent) {
+    event.stopPropagation();
+    this.emit("pointerdown", event, this);
+  }
+
+  onClick(event: MouseEvent) {
+    event.stopPropagation();
+    this.emit("click", event, this);
+  }
+
+  mount(container: HTMLElement) {
+    if (this.element.parentElement) {
+      this.element.parentElement.removeChild(this.element);
+    }
+
+    container.appendChild(this.element);
+
+    Object.values(this.children).forEach((child) => {
+      child.mount(this.element!);
+    });
+  }
+
+  destroy() {
+    this.children.forEach((child) => {
+      child.destroy();
+    });
+
+    this.element.removeEventListener("pointerdown", this.onPointerDown);
+    this.element.remove();
+  }
+
+  reconcile() {
+    this.element.className = `absolute border bg-white ${
+      this.isSelected ? "border-blue-500" : "border-gray-300"
+    }`;
+    this.element.style.width = `${this.width}px`;
+    this.element.style.height = `${this.height}px`;
+    this.element.style.transform = `translate(${this.x}px, ${this.y}px)`;
+  }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,102 @@
 import "./index.css";
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import { Card } from "./Card";
 
-const root = createRoot(document.getElementById("root")!);
+const root = document.getElementById("root")!;
+root.className = "bg-gray-100 w-screen h-screen";
 
-const render = () => {
-  root.render(<App />);
+type ToolType = "pointer" | "card";
 
-  requestAnimationFrame(render);
+const cards: Card[] = [];
+
+let selectedTool: ToolType = "pointer";
+
+let activeCard: Card | null = null;
+let selectedCard: Card | null = null;
+
+const setSelectedCard = (card: Card | null) => {
+  if (selectedCard) {
+    selectedCard.isSelected = false;
+    selectedCard.reconcile();
+  }
+
+  if (card) {
+    selectedCard = card;
+    card.isSelected = true;
+    card.reconcile();
+  }
 };
 
-render();
+const setTool = (tool: ToolType) => {
+  selectedTool = tool;
+
+  if (tool === "card") {
+    setSelectedCard(null);
+    document.body.classList.add("cursor-crosshair");
+  } else {
+    document.body.classList.remove("cursor-crosshair");
+  }
+};
+
+const onKeyPress = (event: KeyboardEvent) => {
+  if (event.code === "KeyC") {
+    setTool("card");
+  }
+};
+
+const onPointerDown = (event: PointerEvent, card?: Card) => {
+  if (event.isPrimary === false) return;
+
+  if (selectedTool === "card") {
+    if (activeCard) {
+      activeCard.isSelected = false;
+      activeCard.reconcile();
+    }
+
+    // todo: handle pointer down on card
+
+    const card = new Card({
+      x: event.clientX,
+      y: event.clientY,
+      width: 0,
+      height: 0,
+    });
+
+    card.on("pointerdown", onPointerDown);
+    card.on("click", onCardClick);
+    card.mount(root);
+    cards.push(card);
+    activeCard = card;
+  }
+};
+
+const onCardClick = (event: MouseEvent, card: Card) => {
+  console.log("card clicked", card);
+  if (selectedTool === "pointer") {
+    setSelectedCard(card);
+  }
+};
+
+const onPointerMove = (event: PointerEvent) => {
+  if (event.isPrimary === false) return;
+
+  if (selectedTool === "card" && activeCard) {
+    activeCard.width = event.clientX - activeCard.x;
+    activeCard.height = event.clientY - activeCard.y;
+    activeCard.reconcile();
+  }
+};
+
+const onPointerUp = (event: PointerEvent) => {
+  if (event.isPrimary === false) return;
+
+  if (selectedTool === "card" && activeCard) {
+    setSelectedCard(activeCard);
+    activeCard = null;
+    setTool("pointer");
+  }
+};
+
+window.addEventListener("keypress", onKeyPress);
+root.addEventListener("pointerdown", onPointerDown);
+root.addEventListener("pointermove", onPointerMove);
+root.addEventListener("pointerup", onPointerUp);
